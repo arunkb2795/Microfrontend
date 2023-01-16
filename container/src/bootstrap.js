6014927a1cb8856aec13823d584dc44d0062d711
arunkb2795 <arunkb27@gmail.com>
2023-01-16 17:54:47 +0530

    Update: Integrating container and marketing

--- container/src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import { mount as productMount } from "products/ProductIndex";
-import { mount as cartMount } from "cart/CartShow";
-
-console.log("Container...");
-
-productMount(document.querySelector("#my-products"));
-cartMount(document.querySelector("#my-carts"));
