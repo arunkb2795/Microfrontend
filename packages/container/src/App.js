1cecae3c051d4ea26d34f2d041be5a0b426f7461
arunkb2795 <arunkb27@gmail.com>
2023-01-19 13:09:11 +0530

    update: yml region

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <div>
-      <h1>Hi there!I'm A container...</h1>
+      <h1>Hi there!I'm A container.</h1>
       <hr></hr>
       <MarketingApp />
     </div>
